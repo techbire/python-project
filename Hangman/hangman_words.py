b5cc6795ec183ecbcf40fa5a407363ccff5b58f9
Ansh Gupta <techbire69@gmail.com>
2023-11-11 22:24:45 +0530

    Add files via upload

---
+++ Hangman/hangman_words.py
@@ -0,0 +1,17 @@
+word_list = [
+'ansh gupta', 
+'abhishek', 
+'techbire', 
+'shrija',
+'raj',
+'anurag', 
+'shivani', 
+'shristi', 
+'khushi', 
+'swati', 
+'anshika', 
+'prakhar',
+'akash',
+'rachit',
+'rishabh',
+]
